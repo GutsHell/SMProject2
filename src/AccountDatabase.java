6ee0996c7efca74500fee4f7ab7fd2ddd3bc9536
Rockorory <electronicwiz01@gmail.com>
2022-02-24 17:50:06 -0500

    initial classes made

---
+++ src/AccountDatabase.java
@@ -0,0 +1,40 @@
+public class AccountDatabase {
+    private Account [] accounts;
+    private int numAcct;
+
+    private int find(Account account) {
+        return -1;
+    }
+
+    private void grow() {
+
+    }
+
+    public boolean open(Account account) {
+        return false;
+    }
+
+    public boolean close(Account account) {
+        return false;
+    }
+
+    public void deposit(Account account) {
+
+    }
+
+    public boolean withdraw(Account account) {
+        return false;
+    }
+
+    public void print() {
+
+    }
+
+    public void printByAccountType() {
+
+    }
+
+    public void printFeeAndInterest() {
+
+    }
+}
--- src/Profile.java
+++ src/Profile.java
@@ -2,4 +2,14 @@ public class Profile {
     private String fname;
     private String lname;
     private Date dob;
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
