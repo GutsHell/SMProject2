85fe804f59b53d00e990958fcc68cc55af1bf10b
Rockorory <electronicwiz01@gmail.com>
2022-02-25 11:31:41 -0500

    completed basic functions of MoneyMarket.java

--- src/MoneyMarket.java
+++ src/MoneyMarket.java
@@ -6,23 +6,23 @@ public class MoneyMarket extends Account {
 
     private int loyalty;
 
-    public MoneyMarket(Profile holder, double balance, int loyalty) {
+    public MoneyMarket(Profile holder, double balance) {
         super(holder, balance);
-        this.loyalty = loyalty;
+        this.loyalty = 1;
     }
 
     @Override
     public double monthlyInterest() {
-        return 0.001;
+        return this.loyalty == 1 ? 0.095/12 : 0.08/12;
     }
 
     @Override
     public double fee() {
-        return 0;
+        return this.balance > 2500 ? 0 : 10;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "MoneyMarket";
     }
 }
