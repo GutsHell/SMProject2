5a7cbac463e3b8e269b5d56471545f301fa22a11
Rockorory <electronicwiz01@gmail.com>
2022-02-25 12:36:52 -0500

    added constructor and completed find(), grow()

--- src/AccountDatabase.java
+++ src/AccountDatabase.java
@@ -5,13 +5,24 @@
 public class AccountDatabase {
     private Account [] accounts;
     private int numAcct;
+    static int NOT_FOUND = -1;
+
+    public AccountDatabase() {
+        this.accounts = new Account[4];
+        numAcct = 0;
+    }
 
     private int find(Account account) {
-        return -1;
+        for (int i = 0; i < numAcct; i++) {
+            if (account.equals(accounts[i])) return i;
+        }
+        return NOT_FOUND;
     }
 
     private void grow() {
-
+        Account [] grown = new Account[accounts.length + 4];
+        System.arraycopy(accounts, 0, grown, 0, accounts.length);
+        this.accounts = grown;
     }
 
     public boolean open(Account account) {
