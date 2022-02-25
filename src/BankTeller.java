8c00867989f237ada000a9c8f7b1d1e9d88dcf6e
Rockorory <electronicwiz01@gmail.com>
2022-02-25 12:34:46 -0500

    completed switch statements for all incoming commands

--- src/BankTeller.java
+++ src/BankTeller.java
@@ -1,3 +1,43 @@
-public class BankTeller {
+import java.util.Scanner;
 
+public class BankTeller {
+    public void run() {
+        AccountDatabase database = new AccountDatabase();
+        Scanner command = new Scanner(System.in);
+        boolean online = true;
+        System.out.print("Bank Teller is running.\n");
+        while (online) {
+            String[] inputAsArray = command.nextLine().split(" ");
+            switch (inputAsArray[0]) {
+                case "O":
+                    //oCommand(inputAsArray, database);
+                    break;
+                case "C":
+                    //cCommand(inputAsArray, database);
+                    break;
+                case "D":
+                    //dCommand(inputAsArray, database);
+                    break;
+                case "W":
+                    //wCommand(inputAsArray, database);
+                    break;
+                case "P":  //pCommand(inputAsArray);
+                    break;
+                case "PT":  //ptCommand(inputAsArray);
+                    break;
+                case "PI":  //piCommand(inputAsArray);
+                    break;
+                case "UB":  //ubCommand(inputAsArray);
+                    break;
+                case "Q":
+                    online = false;
+                    System.out.println("Bank Teller is terminated.");
+                    break;
+                default:
+                    System.out.println("Invalid command!");
+                    break;
+            }
+        }
+        command.close();
+    }
 }
